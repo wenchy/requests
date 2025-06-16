2b75aa4576a7c8b93fc4ac27ea4fa6000280d7e1
Kybxd <627940450@qq.com>
2025-06-16 14:59:55 +0800

    feat: clone http.DefaultTransport

--- env.go
+++ env.go
@@ -2,7 +2,6 @@ package requests
 
 import (
 	"context"
-	"net"
 	"net/http"
 	"time"
 )
@@ -20,21 +19,8 @@ type environment struct {
 var env environment
 
 func init() {
-	env.timeout = 60 * time.Second // default timeout
-	// DefaultTransport
-	// refer: https://github.com/golang/go/blob/c333d07ebe9268efc3cf4bd68319d65818c75966/src/net/http/transport.go#L42
-	env.transport = &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-		}).DialContext,
-		ForceAttemptHTTP2:     true,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-	}
+	env.timeout = 60 * time.Second                                  // default timeout
+	env.transport = http.DefaultTransport.(*http.Transport).Clone() // default transport
 }
 
 // SetEnvTimeout sets the default timeout for each HTTP request at
