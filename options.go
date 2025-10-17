c6790f3debc5777d8ab4a32f1d2fc8f8514e8262
Kybxd <627940450@qq.com>
2025-10-17 10:56:07 +0800

    fix: notes

--- options.go
+++ options.go
@@ -210,8 +210,12 @@ func Body(body io.Reader) Option {
 	}
 }
 
-// Data sets data of request body. It also deduces and sets the Content-Type
-// for the input data.
+// Data sets data of request body. It also deduces Content-Type based on
+// input data types:
+//
+// 1. auto deduce by [http.DetectContentType]: io.Reader, []byte
+// 2. "application/json": struct, slice(except []byte), and map
+// 3. "text/plain": others
 func Data(data any) Option {
 	return func(opts *Options) {
 		opts.Data = data
